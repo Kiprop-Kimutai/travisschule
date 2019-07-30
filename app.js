7953396a87dba149d584314747c20e1a2a69fa86
Kiprop-Kimutai <jonahkiprop34@gmail.com>
2019-07-30 15:40:45 +0300

    #configure app to listen on port assigned from system

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/',(req,res) =>{
 function getServerStatus() {
     return isServerUp;
 }
-const server = app.listen(3000, () =>{
-    console.log('server running on port 3000');
+const server = app.listen(process.env.PORT, () =>{
+    console.log(`server listening on address ${server.address().port}`);
     isServerUp = true;
 })
 
